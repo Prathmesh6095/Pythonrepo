c55c836d2694bc148e689deb463408d022dd1f9d
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-03-08 14:02:22 +0530

    Take user input

--- py2.py
+++ py2.py
@@ -1,6 +1,10 @@
 #Typecasting 
 
-a,b=1,"2";
-c=int(b)
-sum=a+c
-print(sum)
+# a,b=1,"2";
+# c=int(b)
+# sum=a+c
+# print(sum)
+
+a = int(input("Enter a number: "));
+b = int(input("Enter a number: "));
+print(a+b);
