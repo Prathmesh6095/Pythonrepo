03fa1afe9465d724a6a9c38b1c7db2995b52e596
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-03-09 23:03:23 +0530

    Add multiple str function

--- py4.py
+++ py4.py
@@ -11,3 +11,8 @@ str="Prathamesh"
 print(str[1:4])
 print(str[0:4])
 print(str[-3:-1])
+
+print(str.endswith("er"));
+print(str.capitalize());
+print(str.replace("Prathamesh","Patu"));
+print(str.count("th"))
