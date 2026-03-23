d860406c1ce3a562809a4d825f7a89ad25f97240
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-03-23 16:02:08 +0530

    len function used

--- py5.py
+++ py5.py
@@ -3,3 +3,11 @@ str1="This is a string.\twe are creating it in python.";
 print(str1) 
 print(len(str1))
 print(str1[0:40])
+#In python index start from 0
+print(str1[1:])#This means 1 to last character
+print(str1[:47])#This means 0 to 47th character
+print(str1.find("T"))
+
+name = input("Enter your name: ");
+print("Length of your name is: ",len(name))
+
