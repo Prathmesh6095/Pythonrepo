de06005f76bc78bf16e3c35f98f390c7b7f8450a
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-04-07 21:10:38 +0530

    created basic calculator

---
+++ py6.py
@@ -0,0 +1,18 @@
+n1 = int(input("Enter 1st number: "))
+n2 = int(input("Enter 2nd number: "))
+n = input(" + - * / % ")
+    
+    # if n1 and n2 == 0:
+    #     return "invalid number";
+
+if n == "+":
+     print(n1+n2)
+
+elif n == "-":
+    print(n1-n2);
+
+elif n == "*":
+    print(n1*n2);
+
+elif n == "/":
+    print(n1/n2);
