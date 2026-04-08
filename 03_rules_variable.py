29cdf60c0fe9e2217d43c620525279b351799d31
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-04-08 14:39:06 +0530

    Variables declaration rules defined

--- 03_rules_variable.py
+++ 03_rules_variable.py
@@ -8,4 +8,4 @@ sameer = 45
 
 _sameer = 34
 
-# @sameer, s@meer , _sameer this kind of variables are not valid for declaration due to @ symbol
+# @sameer, s@meer , _sameer this kind of variables are not valid for declaration due to @ symbol 
