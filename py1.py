18a4e1c00451ff84c2fb97ec407ed5f8eb55ff6e
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-04-08 14:06:34 +0530

    Again start python from scratch

--- py1.py
+++ py1.py
@@ -1,8 +1,2 @@
-#Write a program in python to get 2 numbers and perform addition on them
-a,b=2,4;
-
-print("Addition is =",a+b)
-print("Subtraction is =",a-b)
-print("Multiplication is =",a*b)
-print("Division is =",a/b)
-print("Modulus is =",a%b)
+#Chapter - 1 (Beginner level)
+print("Hello world")
