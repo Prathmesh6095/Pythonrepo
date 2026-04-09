ac22274a2c8fba3e91f651b72ed7804faaf298ff
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-04-09 15:32:09 +0530

    Taking input

--- 05_type.py
+++ 05_type.py
@@ -7,3 +7,6 @@ a = "31.2"
 b = float(a) # class <float>
 t = type(b) # class <string>
 print(t)
+
+# Typecasting doesn't work like this
+# string to int
---
+++ 06_input.py
@@ -0,0 +1,7 @@
+a = int(input("Enter number 1st : "))
+
+b = int(input("Enter number 2nd : "))
+
+print(a,b)
+print("Sum is:",a+b)
+
