5c34aaa3ab5453101575b6fcfb387f1242fcafb2
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-04-09 15:59:22 +0530

    5th problem solved

---
+++ Practiceset/05_problem.py
@@ -0,0 +1,6 @@
+# Write a python pprogram to find an average of two numbers entered by the user
+m1 = int(input("Enter 1st sub marks: "))
+m2 = int(input("Enter 2nd sub marks: "))
+
+avg = (m1+m2)/2
+print(avg)
