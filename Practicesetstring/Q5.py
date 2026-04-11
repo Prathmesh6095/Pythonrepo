d0a58a09e1afaa26a676338cb9ef96b9a795b4ec
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-04-11 14:45:55 +0530

    Q5 solved

---
+++ Practicesetstring/Q5.py
@@ -0,0 +1,4 @@
+# Write a program to format the following letter using escape sequence characters
+# letter = "Dear Prathamesh, this python course is nice. Thanks!"
+
+print("Dear Prathamesh,\nthis \"python course\" is nice.\nThanks!")
