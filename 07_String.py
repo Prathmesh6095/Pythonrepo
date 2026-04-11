35c778f29dcf040ce0df099437610692d2aa4778
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-04-11 13:46:04 +0530

    Slicing the Strings in python

--- 07_String.py
+++ 07_String.py
@@ -1,4 +1,17 @@
-name1 = "String"
-name2 = 'String1'
-name3 = '''String2'''
-print(name1,name2,name3)
+name1 = "Harry"
+# name2 = 'String1'
+# name3 = '''String2'''
+# print(name1,name2,name3)
+
+# String is immutable
+
+ # String Slicing
+print(name1[-4:-1]) # It will print 0,1,2  
+print(name1[1:4]) # It will print 2,3,
+print(name1[:4]) # It's same as [0:4]
+print(name1[1:])# It's same as [1:5]
+print(name1[1:4:2],"\n")
+
+print(name1.endswith("rry"))
+print(name1.startswith("ha"))
+print(name1.capitalize())
