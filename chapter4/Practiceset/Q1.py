e73a0bb8ec806007f91e6f20c1202583221fd246
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-04-13 15:12:32 +0530

    Practice set solved

--- chapter4/Practiceset/Q1.py
+++ chapter4/Practiceset/Q1.py
@@ -6,4 +6,4 @@ f4 = [input("Enter the names of fruits: ")]
 f5 = [input("Enter the names of fruits: ")]
 f6 = [input("Enter the names of fruits: ")]
 f7 = [input("Enter the names of fruits: ")]
-print(f1,f2,f3,f4,f5,f6,f7)
+print(f1,f2,f3,f4,f5,f6,f7 )
