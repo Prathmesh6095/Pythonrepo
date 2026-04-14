3ae1e5191fcf1ae1ad6b673abf5aa27c2edc8a3e
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-04-14 16:16:43 +0530

    Q5 solved

---
+++ chapter5/Practiceset/Q5.py
@@ -0,0 +1,19 @@
+d = {}
+
+name = input("Enter friend's name: ")
+lang = input("Enter language name: ")
+d.update({name:lang})
+
+name2 = input("Enter friend's name: ")
+lang = input("Enter language name: ")
+d.update({name2:lang})
+
+name3 = input("Enter friend's name: ")
+lang = input("Enter language name: ")
+d.update({name3:lang})
+
+name4 = input("Enter friend's name: ")
+lang = input("Enter language name: ")
+d.update({name4:lang})
+
+print(d)
