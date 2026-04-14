360a779531122c50d219bd037204ed412efbc723
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-04-14 16:19:46 +0530

    Q6 solved

---
+++ chapter5/Practiceset/Q6.py
@@ -0,0 +1,20 @@
+# If languages are same of two friends what will happen to program in problem Q5
+d = {}
+# update() just update it whenever user enters same name
+name = input("Enter friend's name: ")
+lang = input("Enter language name: ")
+d.update({name:lang})
+
+name = input("Enter friend's name: ")
+lang = input("Enter language name: ")
+d.update({name:lang})
+
+name = input("Enter friend's name: ")
+lang = input("Enter language name: ")
+d.update({name:lang})
+
+name = input("Enter friend's name: ")
+lang = input("Enter language name: ")
+d.update({name:lang})
+
+print(d)
