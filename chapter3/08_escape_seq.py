20ce5a9973d7210c0c159933dc2bc6a211460f1e
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-04-14 14:45:59 +0530

    Dictionary in python

---
+++ chapter3/08_escape_seq.py
@@ -0,0 +1,3 @@
+a = "Prathamesh is a \n good boy " # \n is used for new line
+b = "I am a good \"man\""  # \ is used to print quotes in string
+print(a,b)
---
+++ chapter5/01_dict.py
@@ -0,0 +1,8 @@
+marks = {
+    "Prathamesh":100,
+    "Patu":80,
+    "Raju":70
+}
+
+# print(marks, type(marks))
+print(marks["Prathamesh"])
