7887c94479224592515478cc20cfeafc7d1d6afa
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-04-14 15:04:08 +0530

    Different dict methods used

--- chapter5/01_dict.py
+++ chapter5/01_dict.py
@@ -5,4 +5,22 @@ marks = {
 }
 
 # print(marks, type(marks))
-print(marks["Prathamesh"])
+# print(marks["Prathamesh"])
+# print(marks)
+
+# Methods
+
+print(marks.items()) # Prints specific items
+print(marks.keys()) # Prints specified keys "Patu":80 (Patu is key)
+print(marks.values()) # Prints all assigned values "Patu": 80 (80 is value)
+marks.update({"Prathamesh":99}) # Proof of mutability by updation
+print(marks)
+print(marks.get("Patu2")) # prints none
+print(marks["Patu2"]) # returns ann error
+
+# Properties 
+
+# It is unordered.
+# It is mutable.
+# It is indexed.
+# Cannot contain duplicate keys.
