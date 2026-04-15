649581076776e32d82c40210c8b0d9f1e4132878
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-04-15 14:14:23 +0530

    Conditional statements in Python

---
+++ chapter6/01_conditionals.py
@@ -0,0 +1,9 @@
+# In python programming we must be able to execute instructions on a condition(s) being met.
+
+a = int(input("Enter your age: "))
+
+if(a>18):
+    print("You are above the age of concent.")
+
+else:
+    print("You are below the age of concent.")
