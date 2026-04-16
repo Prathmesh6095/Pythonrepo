80d0eadcd23e7185c5f43e4240baf8005c12f63c
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-04-16 12:36:14 +0530

    for Loop in python with iterative

---
+++ chapter7/05_for_loop_iterate.py
@@ -0,0 +1,14 @@
+# For loops with String
+s = "Patu"
+for i in s:
+    print(i) # Prints every single character from string
+
+# For loops with list
+l = [1,4,6,234,6,764]
+for i in l:
+    print(i)
+
+# For loops with tuples
+t = (6, 231, 75, 122)
+for i in t:
+    print(i)
