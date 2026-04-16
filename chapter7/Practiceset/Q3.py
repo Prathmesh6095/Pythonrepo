06753d7cd6fd74d541997426d75b12f33a06feb9
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-04-16 13:08:10 +0530

    Q3 solved

---
+++ chapter7/Practiceset/Q3.py
@@ -0,0 +1,12 @@
+# Attempt problem no.1 with while loop
+n = int(input("Enter a number: "))
+
+i = 1
+
+
+while(i<11):
+    print(f"{n} X {i} = {n*i}")
+    i += 1
+
+# for i in range(1,11):
+#     print(f"{n} X {i} = {n*i}")
