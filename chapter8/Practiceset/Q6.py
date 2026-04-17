a890fa2b438908dc74270312eb87c28404a337e8
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-04-17 14:32:47 +0530

    Q6 solved

---
+++ chapter8/Practiceset/Q6.py
@@ -0,0 +1,9 @@
+# Write a python function which converts inches to cms.
+# Formula: inch * 2.54
+
+def inch_to_cms(inch):
+    return inch * 2.54
+
+n = int(input("Enter the value in inches: "))
+
+print(f"The corresponding value in cms is {inch_to_cms(n)}")
