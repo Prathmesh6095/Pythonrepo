7bdbb6fbc496147d3fbcc547e618ff61b99e15e3
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-04-17 13:49:32 +0530

    Functions with default arg in Python

---
+++ chapter8/04_default_arg.py
@@ -0,0 +1,7 @@
+def goodday(name, ending = "Thank you!"):
+    print(f"Good Day, {name}")
+    print(ending)
+    
+
+goodday("Pathu!", "Thanks") # Here Thanks is ending value
+goodday("Rohan")  # Here there's no ending value so by default, it will take "Thank you" as default value
