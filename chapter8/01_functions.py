e2c6a6857c2efffa07e6ce3cc26816fd3c08120d
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-04-17 13:22:38 +0530

    Functions in Python

---
+++ chapter8/01_functions.py
@@ -0,0 +1,15 @@
+# a = 12
+# b = 45
+# c = 56
+
+# avg = (a + b + c)/3
+# print(avg)
+
+def avg():
+        a = int(input("Enter your number: "))
+        b = int(input("Enter your number: "))
+        c = int(input("Enter your number: "))
+        avg = (a + b + c)/3
+        print(avg)
+
+avg()
