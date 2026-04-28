a6cf154e6782ed3006a919ec8c563b052d890783
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-04-28 10:13:33 +0530

    Single Inheritance

---
+++ chapter11/01_inheritance.py
@@ -0,0 +1,16 @@
+# Single Inheritance
+
+class Employee:
+    company = "ITC"
+    def show(self):
+        print(f"The name is {self.name} and the salary is {self.salary}")
+
+class Programmmer(Employee):
+    company = "ITC Infotech"
+    def showLanguage(self):
+        print(f"The name is {self.name} and he is good with {self.language} language.")
+
+a = Employee()
+b = Programmmer()
+
+print(a.company, b.company)
