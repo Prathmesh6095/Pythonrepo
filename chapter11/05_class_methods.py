6ba5b897abe2c6e287fff06803cc632a4db9e89f
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-04-29 14:24:37 +0530

    Make it more clear

--- chapter11/05_class_methods.py
+++ chapter11/05_class_methods.py
@@ -1,12 +1,11 @@
 class Employee:
     a = 1
-    @classmethod
+    @classmethod # Class methos is a method where you can access class attribute directly
     def show(cls):
-        print(f"The class attribute of a is {cls.a}")
+        print(f" The class attribute of a is {cls.a}")
 
 e = Employee()
 e.a = 45
 
 e.show()
 
-# Class methos is a method where you can access class attribute directly
