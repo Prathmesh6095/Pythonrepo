2d74e652249e93392fe48febd0e5f54969682990
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-05-06 11:46:34 +0530

    salary and name added in example

--- chapter11/01_inheritance.py
+++ chapter11/01_inheritance.py
@@ -2,15 +2,18 @@
 
 class Employee:
     company = "ITC"
+    name = "Patu"
+    salary = 100000
     def show(self):
         print(f"The name is {self.name} and the salary is {self.salary}")
 
 class Programmmer(Employee):
-    company = "ITC Infotech"
+    company = "ITC Infotech "
+    language = "Py"
     def showLanguage(self):
         print(f"The name is {self.name} and he is good with {self.language} language.")
 
 a = Employee()
 b = Programmmer()
 
-print(a.company, b.company)
+print(a.company,a.name)
