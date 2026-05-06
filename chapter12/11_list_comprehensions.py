6ab12a031e8b947ea5b6d9196657303b0651e670
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-05-06 15:46:08 +0530

    Advanced python list comprehensions

---
+++ chapter12/11_list_comprehensions.py
@@ -0,0 +1,10 @@
+mylist = [1, 2, 3, 5, 9, 8]
+
+# squaredlist = []
+# for item in mylist:
+#     sqaredlist.append(item*item)
+
+# Simplify
+squaredlist = [i*i for i in mylist]
+
+print(squaredlist)
