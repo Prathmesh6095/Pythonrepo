72635ca3b05668c4f2a6a660028786c0c6b7b1b6
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-05-06 15:14:18 +0530

    Advanced python excetions

---
+++ chapter12/04_exception.py
@@ -0,0 +1,14 @@
+try:
+    a = int(input("Hey, Enter a number: "))
+
+    print(a)
+
+except ValueError as v:
+    print(v)
+
+except TypeError:
+    print("Type error")
+
+except Exception as e:
+    print(e)
+
